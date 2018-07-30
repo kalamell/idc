1a2900ea10243158e0f929392c2afcaeacd831d6
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-07-30 22:10:41 +0700

    edit ignore

--- index.php
+++ index.php
@@ -56,9 +56,9 @@ ini_set('memory_limit', -1);
  * NOTE: If you change these, also change the error_reporting() code below
  */
 
-//define('APP_URL', ($_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http') . "://{$_SERVER['SERVER_NAME']}".str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']));
+define('APP_URL', ($_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http') . "://{$_SERVER['SERVER_NAME']}".str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']));
 
-define('APP_URL', 'http://thai-idc.com');
+//define('APP_URL', 'http://thai-idc.com')
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 
